d59f26183dc5b9d1614e039d6ab28aa32554dfe1
Robert Smith <robertsmith1@Roberts-MacBook-Pro.local>
2017-03-05 09:58:27 -0800

    updated site files

--- assets/javascript/nytimes.js
+++ assets/javascript/nytimes.js
@@ -1,20 +1,7 @@
 //AuthKey used for NYT API
 var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 //
-//var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
- //authKey + "&q=";
 
- //Search term
-
- //Number of queries
-
- //Start year
-
- //End Year
-
- //Search button
-
- //Clear results
 
 $(document).ready(function() {
 
@@ -39,12 +26,38 @@ $(document).ready(function() {
 		  'end_date': eDate
 		});
 
+		//Prevents page from reloading so we dont lose data
+		e.preventdefault();
+
+		//AJAX to retrieve NYT API data
 		$.ajax({
 		  url: url,
 		  method: 'GET',
 		  
-		}).done(function(result) {
-		  console.log(result);
+		}).done(function(response) {
+		  //Log the response to get object data
+		  console.log(response);
+
+		  var results = response.response.docs;
+
+		  //For loops for the array
+		  for(var i = 0; i < results.length; i++) {
+		  	var count = 0;
+		  	var newSpan = $('<span>').addClass('numForList');
+		  	var newDiv = $('<div>').addClass('resultsDiv');
+		  	var titleP = $('<p>');
+		  	var abstractP = $('<p>').addClass('fontSub');
+		  	var newOl = $('<ol>');
+		  	var newList = $('<li>');
+		  	var webURL = results[i].webURL;
+		  	var titleA = $('<a>').attr('href', webURL);
+		  	var articleTitle = results[i].headline.main;
+		  	var articleAbstract = results[i].abstract;
+		  	var pubDate = results[i].pub_date;
+		  }
+
+
+
 
 		}).fail(function(err) {
 		  throw err;
